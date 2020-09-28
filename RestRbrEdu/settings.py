8e717c2186f3ca574f9ed2e5c20daaf29ce0ef7e
herman72 <m.sherafati7@gmail.com>
2020-09-28 12:12:58 +0330

    Create first app of Rest and test it

--- RestRbrEdu/settings.py
+++ RestRbrEdu/settings.py
@@ -36,7 +36,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'quickstart',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
